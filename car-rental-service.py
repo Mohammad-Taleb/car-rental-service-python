a5ad89769701970032f9a9b35bdd4945ab322dc7
Mohammad-Taleb <akamohdtaleb@gmail.com>
2025-03-01 13:59:52 +0200

    Add child classes car and bike <> to have their own and the inherited attributes

--- car-rental-service.py
+++ car-rental-service.py
@@ -12,3 +12,13 @@ class Vehicle:
   
 
 
+class Car(Vehicle):
+  def __init__(self, brand, model, year, rental_price_per_day, seating_capacity):
+    super().__init__(brand, model, year, rental_price_per_day)
+    self.seating_capacity = seating_capacity
+
+
+class Bike(Vehicle):
+  def __init__(self, brand, model, year, rental_price_per_day, engine_capacity):
+    super().__init__(brand, model, year, rental_price_per_day)
+    self.engine_capacity = engine_capacity
