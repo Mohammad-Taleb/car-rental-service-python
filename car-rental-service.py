c73e869affd9f12535c587b6a8a93f79a3d4c2b5
Mohammad-Taleb <akamohdtaleb@gmail.com>
2025-03-01 13:51:47 +0200

    Add parent class Vehicle <> to include the main attributes and methods

---
+++ car-rental-service.py
@@ -0,0 +1,14 @@
+class Vehicle:
+  def __init__(self, brand, model, year, rental_price_per_day):
+    self.brand = brand
+    self.model = model
+    self.year = year
+    self.__rental_price_per_day = rental_price_per_day
+
+
+
+  def display_info(self):
+    print(f"Brand: {self.brand}, Model: {self.model}, Year: {self.year}, Rental Price per Day: ${''}")
+  
+
+
