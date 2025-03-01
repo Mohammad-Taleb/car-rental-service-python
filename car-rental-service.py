5b968c35cea61f90c8d3d84e8788a1f779f5ab53
Mohammad-Taleb <akamohdtaleb@gmail.com>
2025-03-01 14:08:11 +0200

    Add getter and setter methods <> to update and display the rental price

--- car-rental-service.py
+++ car-rental-service.py
@@ -5,10 +5,20 @@ class Vehicle:
     self.year = year
     self.__rental_price_per_day = rental_price_per_day
 
+  def get_rental_price_per_day(self):
+    return self.__rental_price_per_day
+  
+
+  def set_rental_price_per_day(self, price):
+    if price > 0:
+      self.__rental_price_per_day = price
+    else:
+      print("Price should be greater than ZERO")
+
 
 
   def display_info(self):
-    print(f"Brand: {self.brand}, Model: {self.model}, Year: {self.year}, Rental Price per Day: ${''}")
+    print(f"Brand: {self.brand}, Model: {self.model}, Year: {self.year}, Rental Price per Day: ${self.get_rental_price_per_day()}")
   
 
 
